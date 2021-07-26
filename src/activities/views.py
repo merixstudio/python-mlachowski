c9a7d1b4a838f09b77b5a680b3e037afe5f64086
Tomasz Szymański <t.szymanski@merixstudio.com>
2021-07-26 22:24:57 +0200

    view update

--- src/activities/views.py
+++ src/activities/views.py
@@ -1,8 +1,15 @@
+import requests
+from django.forms import model_to_dict
 from rest_framework import filters, viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from activities.models import Activity
+from activities.models import Activity, ACTIVITY_TYPES
 from activities.serializers import ActivitySerializer
+
+
+BORED_API_URL = "https://www.boredapi.com/api/"
 
 
 class ActivityViewSet(viewsets.ModelViewSet):
@@ -22,3 +29,25 @@ class ActivityViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         is_performed = self.request.query_params.get("is_performed", False)
         return self.queryset.filter(is_performed=is_performed)
+
+
+class ActivityGenerateViewSet(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        response = requests.get(BORED_API_URL + "activity/")
+        generated_activity = response.json()
+
+        for key, value in dict(ACTIVITY_TYPES).items():
+            if value == generated_activity["type"]:
+                activity_type = key
+
+        activity = Activity(
+            description=generated_activity["activity"],
+            type=activity_type,
+            number_of_participants=generated_activity["participants"],
+            cost=int(generated_activity["price"] * 10),
+        )
+        activity.save()
+
+        return Response(model_to_dict(activity))
